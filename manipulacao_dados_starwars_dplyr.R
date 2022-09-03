7f78d78ea8848995affdafb707e84ebe32a59098
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-09-02 21:33:42 -0300

    Add files via upload

--- manipulacao_dados_starwars_dplyr.R
+++ manipulacao_dados_starwars_dplyr.R
@@ -17,10 +17,32 @@ View(wars)
 
 ## Principais funções do dplyr
 
-# 1. select (ends_with; starts_with; contains; matches)
-# 2. filter (==; =!; |; &; xor(); %in%; >; <; >=; <=)
+# 1. select (ends_with; starts_with; contains; matches; where)
+# 2. filter (==; =!; |; &; xor(); %in%; >; <; >=; <=; any; all; slice_max; slice_min; between; near)
 # 3. rename
 # 4. group_by
 # 5. arrange (desc)
 # 6. summarise
 
+## select
+
+starwars %>%
+  select(ends_with("color"))
+
+starwars %>%
+  select(starts_with("h"))
+
+starwars %>%
+  select(contains("h"))
+
+starwars %>%
+  select(matches("mass"))
+
+starwars %>%
+  select(matches("year"))
+
+starwars %>%
+  select(matches("color"))
+
+starwars %>%
+  select(where(is.numeric))
